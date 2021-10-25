4d856012bb00fe228e0df45fffba20fb2e10b0e8
Ahmed Sultan <ahmedsoultan2000@gmail.com>
2021-10-25 21:13:36 +0200

    Initial Commit

---
+++ The Cat App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Cat App
+//
+//  Created by ahmed sultan on 25/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ The Cat AppUITests/The_Cat_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  The_Cat_AppUITestsLaunchTests.swift
+//  The Cat AppUITests
+//
+//  Created by ahmed sultan on 25/10/2021.
+//
+
+import XCTest
+
+class The_Cat_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
